4cc3dc7d3facd9d57f984286ed6f7b197d26d5b5
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-16 13:33:59 -0500

    change all to double quotes

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -1,9 +1,9 @@
 test_that("linalg and optim least-sq coincide", {
   n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  data <- simulate_data(n_obs, n_pred, model = "linear", seed = 1918)
   design <- data$design; outcome <- data$outcome
-  via_linalg_out <- hiper_glm(design, outcome, model = 'linear')
-  via_bfgs_out <- hiper_glm(design, outcome, model = 'linear', options = list(mle_solver = 'BFGS'))
+  via_linalg_out <- hiper_glm(design, outcome, model = "linear")
+  via_bfgs_out <- hiper_glm(design, outcome, model = "linear", options = list(mle_solver = "BFGS"))
 
   ## compare analytical with BFGS
   expect_true(are_all_close(
