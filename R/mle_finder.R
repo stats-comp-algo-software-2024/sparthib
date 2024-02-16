67f158fb9b971843b6cc9233e79e70c31f4113ef
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-16 13:19:31 -0500

    use stats::optim for BFGS

--- R/mle_finder.R
+++ R/mle_finder.R
@@ -3,64 +3,30 @@ find.mle.pseudoinv <- function(design, outcome){
   return(betas)
 }
 
+
+
+
+min_residuals <- function(data = df, par) {
+
+  design <- as.matrix(df[, names(df) != "outcome"])
+  res <- design%*%par - as.matrix(df$outcome)
+  res_sq <- t(res)%*%res
+  return(res_sq)
+}
+
+
 find.mle.bfgs <- function(design, outcome, tol=1e-6){
-  betas <- rep(0, ncol(design))
-
-  loglik <- loglik.linear(design, outcome, betas)
-  grad <- grad.linear(design, outcome, betas)
-
-  hessian <- t(design) %*% design
-
-  n <- dim(design)[[2]]
-  H <- diag(n)
-
-  serach_direction <- -H %*% grad
-
-  rho_backtracking <- 0.95
-  c <- 0.9
 
 
-  n_iter <- 0
+  design <- cbind(rep(1, dim(design)[1]), design)
+  df <- as.data.frame(design)
+  par <- as.matrix(rep_len(0.001, ncol(design)))
+  df$outcome <- outcome
 
-  while(norm(grad, "2") > tol){
-    search_direction <- -H %*% grad
-    step_size <- 1
+  result <- stats::optim(par = par, fn=min_residuals, data=df,
+               method = "BFGS")
+  return(result$par)
 
-    lhs <- loglik.linear(design, outcome, betas + step_size * search_direction)
-    rhs <- loglik + c * step_size %*% t(grad) %*% search_direction
-
-    print(lhs)
-    print(rhs)
-
-    while (lhs > rhs) {
-
-      step_size <- rho_backtracking * step_size
-      # update
-      lhs <- loglik.linear(design, outcome, betas + (step_size * search_direction))
-      rhs <- loglik + c * step_size %*% t(grad) %*% search_direction
-
-    }
-
-    betas_new <- betas + step_size * search_direction
-
-    grad_new <- grad.linear(design, outcome, betas_new)
-
-    s <- betas_new - betas
-    y <- grad_new - grad
-
-    rho <- 1 / (t(y) %*% s) %>%
-      c()
-    H <- (diag(n) - rho * s %*% t(y)) %*% H %*% (diag(n) - rho * y %*% t(s)) + rho * s %*% t(s)
-
-    betas <- betas_new
-    grad <- grad_new
-
-    n_iter <- n_iter + 1
-
-
-  }
-
-  print(n_iter)
-  return(betas)
 
 }
+
