6267a17a84a895b23c2cf3fb92c1ca7bc71954ab
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-19 13:51:02 -0500

    update `test-mle-finder.R` to compare newton vs bfgs outputs

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -1,26 +1,25 @@
-test_that("analytical and BFGS coincide", {
+test_that("linalg and optim least-sq coincide", {
   n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = "linear", seed = 1918)
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
   design <- data$design; outcome <- data$outcome
-  via_linalg_out <- hiper_glm(design, outcome, model = "linear", options = list(mle_solver = "LS"))
-  via_bfgs_out <- hiper_glm(design, outcome, model = "linear", options = list(mle_solver = "BFGS"))
-
-  ## compare analytical with BFGS
-  expect_true(are_all_close(coef(via_linalg_out), coef(via_bfgs_out)))
-
+  via_linalg_out <- hiper_glm(design, outcome, model = 'linear')
+  via_bfgs_out <- hiper_glm(
+    design, outcome, model = 'linear', option = list(mle_solver = 'BFGS')
+  )
+  expect_true(are_all_close(
+    coef(via_linalg_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+  ))
 })
 
-test_that("grad and numerical grad", {
+test_that("newton and bfgs outputs coincide on logit model", {
   n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = "linear", seed = 1918)
-  design <- data$design
-  outcome <- data$outcome
-  betas <- hiper_glm(design, outcome, model = "linear", options = list(mle_solver = "LS"))$coefficients
-  grad <- grad_linear(design, outcome, betas)
-  loglik <- loglik_linear(design, outcome, betas)
-  approxgrad  <- approxgrad_linear(loglik, )
-
-
-  expect_true(are_all_close( grad_linear, approxgrad_linear))
-
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_bfgs_out <- hiper_glm(
+    design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
+  )
+  expect_true(are_all_close(
+    coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+  ))
 })
