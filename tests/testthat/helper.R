ec962449fa91386d328aa57b4285122425aa919a
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-21 14:37:50 -0500

    add Newton Raphson MLE finder for logistic, and test function also test difference between approx and analytical grad for log logistic likelihood

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -68,19 +68,10 @@ simulate_data <- function(
   return(list(design = design, outcome = outcome, coef_true = coef_true))
 }
 
-loglik_linear <- function(design, outcome, betas){
-  mat <- outcome - design %*% betas
-  loglik <- 0.5*t(mat) %*% (mat)
-  return(loglik)
-}
 
-grad_linear <- function(design, outcome, betas){
-  grad <- -t(design) %*% (outcome - design %*% betas)
-  return(grad)
-}
 
 approxgrad_linear <- function(func, x, dx = .Machine$double.eps^(1/3)) {
-  numerical_grad <- rep(0, length(x))
+  grad <- rep(0, length(x))
   for (i in 1:length(x)){
     x_plus <- x
     x_plus[[i]] <- x_plus[[i]] + dx
@@ -88,8 +79,9 @@ approxgrad_linear <- function(func, x, dx = .Machine$double.eps^(1/3)) {
     x_minus <- x
     x_minus[[i]] <- x_minus[[i]] - dx
 
-    numerical_grad[[i]] <- (func(x_plus) - func(x_minus))/(2*dx)
+    grad[[i]] <- (func(x_plus) - func(x_minus))/(2*dx)
   }
 
-  return(numerical_grad)
+  return(grad)
 }
+
--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -2,7 +2,7 @@ test_that("linalg and optim least-sq coincide", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
   design <- data$design; outcome <- data$outcome
-  via_linalg_out <- hiper_glm(design, outcome, model = 'linear')
+  via_linalg_out <- hiper_glm(design, outcome, model = 'linear', option = list(mle_solver = 'LS'))
   via_bfgs_out <- hiper_glm(
     design, outcome, model = 'linear', option = list(mle_solver = 'BFGS')
   )
@@ -11,11 +11,41 @@ test_that("linalg and optim least-sq coincide", {
   ))
 })
 
+
+test_that("analytical vs approx grad linear ", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  betas <- c(1,2,3,4)
+  approx <- approxgrad_linear(function(x) loglik_linear(design, outcome, x),
+                     betas)
+  analytical <- grad_linear(design,outcome, betas)
+  expect_true(are_all_close(
+    approx, analytical, abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+  })
+
+
+test_that("analytical vs approx grad logistic ", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  betas <- c(1,2,3,4)
+  approx <- approxgrad_linear(function(x) loglik_logistic(design, outcome, x),
+                              betas)
+  analytical <- numerical_grad_logistic(design,outcome, betas)
+  expect_true(are_all_close(
+    approx, analytical, abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
+
+
+
 test_that("newton and bfgs outputs coincide on logit model", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
   design <- data$design; outcome <- data$outcome
-  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_newton_out <- hiper_glm(design, outcome, model = 'logit', option = list(mle_solver = 'NR'))
   via_bfgs_out <- hiper_glm(
     design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
   )
